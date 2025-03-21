dbaccc4335d9a86f3094031be3e7d8c69d7310e2
hnawazatcodezac <hamid.nawaz@codezac.com>
2025-03-21 22:25:21 +0500

    COD-push

--- backend/src/routes/merchant/coupon.js
+++ backend/src/routes/merchant/coupon.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const {
   createCoupon,
-} = require("../../controllers/admin/coupon.js");
+} = require("../../controllers/merchant/coupon.js");
 const { verifyMerchantToken } = require("../../middlewares/authMiddleware.js");
 const { paramsValidator, bodyValidator } = require("../../middlewares/joi.js");
 
