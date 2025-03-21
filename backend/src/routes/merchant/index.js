dab4ae31029dac041747e604e474c48486e9e5bd
hnawazatcodezac <hamid.nawaz@codezac.com>
2025-03-21 23:03:57 +0500

    COD-push

--- backend/src/routes/merchant/index.js
+++ backend/src/routes/merchant/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const labelRoutes = require("./label");
+const couponRoutes = require("./coupon");
 const orderRoutes = require("./order");
 const settingRoutes = require("./setting");
 const categoryRoutes = require("./category");
@@ -9,6 +10,7 @@ const reviewRoutes = require("./review");
 const productRoutes = require("./product");
 
 router.use("/order", orderRoutes);
+router.use("/coupon", couponRoutes);
 router.use("/product", productRoutes);
 router.use("/setting", settingRoutes);
 router.use("/category", categoryRoutes);
